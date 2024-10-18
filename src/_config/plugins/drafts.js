fcc3a9cb66da629de7842572fad8ed795ce16290
Guido Rota <guido.rota@gmail.com>
2024-10-18 15:52:19 +1100

    Use preprocessor API for draft mode

--- src/_config/plugins/drafts.js
+++ src/_config/plugins/drafts.js
@@ -1,30 +1,12 @@
 export const drafts = eleventyConfig => {
-  eleventyConfig.addGlobalData('eleventyComputed.permalink', function () {
-    return data => {
-      // Always skip during non-watch/serve builds
-      if (data.draft && !process.env.BUILD_DRAFTS) {
-        return false; // Ensure templates that use this handle it correctly
-      }
-      return data.permalink;
-    };
-  });
-
-  // When `eleventyExcludeFromCollections` is true, the file is not included in any collections
-  eleventyConfig.addGlobalData('eleventyComputed.eleventyExcludeFromCollections', function () {
-    return data => {
-      // Always exclude from non-watch/serve builds
-      if (data.draft && !process.env.BUILD_DRAFTS) {
-        return true;
-      }
-
-      return data.eleventyExcludeFromCollections ?? false;
-    };
-  });
-
-  eleventyConfig.on('eleventy.before', ({runMode}) => {
-    // Set the environment variable
-    if (runMode === 'serve' || runMode === 'watch') {
-      process.env.BUILD_DRAFTS = true;
+  eleventyConfig.addPreprocessor("drafts", "*", (data, _) => {
+    if (data.draft && isProductionBuild()) {
+      return false;
     }
   });
 };
+
+function isProductionBuild() {
+  return process.env.ELEVENTY_ENV === 'production'
+    && process.env.ELEVENTY_RUN_MODE === 'build';
+}
