45b5e576b0fe4b8735638eb2ab940f0cca5d739e
Guido Rota <guido.rota@gmail.com>
2024-10-21 20:16:42 +1100

    Various changes to opengraph image conversion to jpg

--- src/_config/events/svg-to-jpeg.js
+++ src/_config/events/svg-to-jpeg.js
@@ -2,32 +2,29 @@ import {promises as fsPromises, existsSync} from 'node:fs';
 import path from 'node:path';
 import Image from '@11ty/eleventy-img';
 
-const ogImagesDir = './src/assets/og-images';
+const opengraphPreviewImagesDir = 'dist/assets/og-images/';
 
 export const svgToJpeg = async function () {
-  const socialPreviewImagesDir = 'dist/assets/og-images/';
 
-  if (!existsSync(socialPreviewImagesDir)) {
-    console.log('⚠ No OG images dir found');
+  if (!existsSync(opengraphPreviewImagesDir)) {
+    console.log('No OpenGraph images dist directory found');
     return;
   }
 
-  const files = await fsPromises.readdir(socialPreviewImagesDir);
-  if (files.length > 0) {
-    files.forEach(async function (filename) {
-      const outputFilename = filename.substring(0, filename.length - 4);
-      if (filename.endsWith('.svg') & !existsSync(path.join(ogImagesDir, outputFilename))) {
-        const imageUrl = socialPreviewImagesDir + filename;
-        await Image(imageUrl, {
-          formats: ['jpeg'],
-          outputDir: ogImagesDir,
-          filenameFormat: function (id, src, width, format, options) {
-            return `${outputFilename}.${format}`;
-          }
-        });
+  const files = await fsPromises.readdir(opengraphPreviewImagesDir);
+  if (files.length == 0) {
+    console.log('No images found on OG images dir');
+    return;
+  }
+
+  files.forEach(async function (filename) {
+    const imageUrl = opengraphPreviewImagesDir + filename;
+    await Image(imageUrl, {
+      formats: ['jpeg'],
+      outputDir: opengraphPreviewImagesDir,
+      filenameFormat: function (id, src, width, format, options) {
+        return `${path.parse(src).name}.${format}`;
       }
     });
-  } else {
-    console.log('⚠ No images found on OG images dir');
-  }
+  });
 };
