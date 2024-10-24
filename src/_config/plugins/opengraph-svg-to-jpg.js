0308cac30feb61b81dfb3169ec498b73462cd387
Guido Rota <guido.rota@gmail.com>
2024-10-24 21:17:58 +1100

    Increase Open Graph image quality and fix formatting in svg->jpeg image converter plugin.

--- src/_config/plugins/opengraph-svg-to-jpg.js
+++ src/_config/plugins/opengraph-svg-to-jpg.js
@@ -7,27 +7,33 @@ export const opengraphSvgToJpg = (eleventyConfig) => {
 };
 
 const svgToJpeg = async function () {
-    const opengraphPreviewImagesDir = 'dist/assets/og-images/';
+  const opengraphPreviewImagesDir = 'dist/assets/og-images/';
 
-if (!existsSync(opengraphPreviewImagesDir)) {
-    console.log('No OpenGraph images dist directory found');
-    return;
-}
+  if (!existsSync(opengraphPreviewImagesDir)) {
+      console.log('No OpenGraph images dist directory found');
+      return;
+  }
 
-const files = await fsPromises.readdir(opengraphPreviewImagesDir);
-if (files.length == 0) {
-    console.log('No images found on OG images dir');
-    return;
-}
+  const files = await fsPromises.readdir(opengraphPreviewImagesDir);
+  if (files.length == 0) {
+      console.log('No images found on OG images dir');
+      return;
+  }
   
-    files.forEach(async function (filename) {
-      const imageUrl = opengraphPreviewImagesDir + filename;
-      await Image(imageUrl, {
-        formats: ['jpeg'],
-        outputDir: opengraphPreviewImagesDir,
-        filenameFormat: function (id, src, width, format, options) {
-          return `${path.parse(src).name}.${format}`;
-        }
-      });
-    });
-  };
+  files.forEach(async function (filename) {
+    const imageUrl = opengraphPreviewImagesDir + filename;
+    
+    const imageConversionOptions = {
+      formats: ['jpeg'],
+      outputDir: opengraphPreviewImagesDir,
+      filenameFormat: function (id, src, width, format, options) {
+        return `${path.parse(src).name}.${format}`;
+      },
+      sharpJpegOptions: {
+        quality: 95
+      }
+    };
+
+    await Image(imageUrl, imageConversionOptions);
+  });
+};
